a6ce579befb856696f3aff59893bc3926dab24ae
Lesliechan201@gmail.com <Lesliechan201@gmail.com>
2026-07-16 17:37:50 -0400

    fix

--- StarLevelSystem/StarLevelSystem.cs
+++ StarLevelSystem/StarLevelSystem.cs
@@ -26,7 +26,7 @@ namespace StarLevelSystem
     {
         public const string PluginGUID = "MidnightsFX.StarLevelSystem";
         public const string PluginName = "StarLevelSystem";
-        public const string PluginVersion = "1.1.2";
+        public const string PluginVersion = "1.1.4";
 
         public ValConfig cfg;
         // Use this class to add your own localization to the game
